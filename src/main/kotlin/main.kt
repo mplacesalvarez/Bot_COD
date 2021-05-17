b94c3f2867255766e7cd6ef5fe28f1e07ea92ce3
mplacesalvarez <mplacesalvarez@danielcastelaor.org>
2021-05-17 20:33:19 +0200

    Cambios en build.gradle.kts

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
+
+import com.github.kotlintelegrambot.bot
+import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.text
+import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.ParseMode
+import com.github.kotlintelegrambot.network.fold
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+
 }
